07d82806dde120e0779ff69965533d9a97ef2872
Shubhdeep Singh <shubhdeepsingh1502@gmail.com>
2024-07-03 22:49:16 +0530

    Update App.jsx

--- interactive_app/Frontend/src/App.jsx
+++ interactive_app/Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import Home from "./pages/Home";
-import ProfilePage from './profile/ProfilePage';
+import ProfilePage from "./profile/ProfilePage";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/Home" element={<Home />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
       </BrowserRouter>
     </>
